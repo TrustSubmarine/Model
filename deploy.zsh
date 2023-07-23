0ead6f91a8bc647f93f4d366cf5df5ff9ccaa310
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2023-07-23 23:33:45 +0530

    Fix zip bug in deploy.zsh

--- deploy.zsh
+++ deploy.zsh
@@ -1,2 +1,2 @@
 # zip to upload to AWS Lambda
-zip archive.zip ./venv/lib/python3.9/site-packages/* main.py model.pkl
+(zip archive.zip main.py model.pkl) && (cd venv/lib/python3.9/site-packages && zip -r "$OLDPWD/archive.zip" .)
